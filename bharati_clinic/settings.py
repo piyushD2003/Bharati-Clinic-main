a4e00e1a7d52d5ebdc4d02038edd91d7644f83bd
Piyush Dhyani <piyushdhyani7@gmail.com>
2025-03-13 16:50:50 +0530

    netlify.app allow hosts

--- bharati_clinic/settings.py
+++ bharati_clinic/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".vercel.app", "127.0.0.1"]
+ALLOWED_HOSTS = [".vercel.app", ".netlify.app", "127.0.0.1"]
 
 
 # Application definition
